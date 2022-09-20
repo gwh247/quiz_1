13aaf5755a5f0b221d95ff135d0ee31d405b5eee
gwh247 <110586109+gwh247@users.noreply.github.com>
2022-09-20 13:40:44 -0500

    Add files via upload

---
+++ createMarketingList.py
@@ -0,0 +1,76 @@
+# Read the file VendorList.csv into this program and create a dictionary of the customer's
+# full name, email address and phone number where the key is the full name of the customer
+# and the value is a dictionary where the keys are 'email' and 'phone' and the values
+# are the corresponding email address and phone number of the customer. 
+
+# Once the dictionary has been completed print it out. It shoud resemble what is shown
+# below (first 2 and last 2 elements shown only):
+
+#{'Tommie Goody': {'email': 'tgoody0@weather.com', 'phone': '809-992-7298'}, 
+# 'Obadiah Godfery': {'email': 'ogodfery1@a8.net', 'phone': '560-745-9361'}......
+# ..........'Kessiah Tynemouth': {'email': 'ktynemouthdu@ning.com', 'phone': '690-215-8097'}, 
+# 'Carmela Kaubisch': {'email': 'ckaubischdv@wikia.com', 'phone': '307-726-6526'}}
+
+
+# Using the dictionary, write the contents to a csv file. The output file shoud be exactly as
+# what is shown in the file - marketinglist.csv.
+# Name your file - marketinglistFINAL.csv
+
+
+# Note: you can use the comments below to guide you through the logic of the code. You are not
+# required to follow it. ALSO NOT ALL STEPS HAVE BEEN COMMENTED. You may have additional steps.
+
+
+import csv
+from fileinput import close
+
+# open the vendorlist file
+infile = open('VendorList.csv','r')
+csvfile = csv.reader(infile)
+next(csvfile)
+
+# create a csv object from the file object
+
+
+
+# create an output file
+outfile = open('marketinglistFinal.csv', 'w')
+writer = csv.writer(outfile)
+
+
+
+
+# create an empty dictionary
+dict1 = []
+
+
+# iterate through the csv object   id,first_name,last_name,gender,email,phone
+for record in csvfile: 
+    full_name = int(record[1]) + " " + int(record[2])
+    email = int(record[4])
+    phone_num = int(record[5])
+   
+
+
+
+    # add the key-value pair to the dictionary
+    dict1['full_name'] = [full_name,email, phone_num]
+
+
+
+# print the dictionary after the loop is finished
+print(dict1)
+
+
+
+# iternate through the dictionary and write to the output file
+for record in dict1:
+    writer.writerow(dict1[0,1,2])
+    
+
+
+
+
+# close your output file
+outfile.close 
+
